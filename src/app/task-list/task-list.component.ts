9ad0a17f25ab06a64af45899bc49b3ff90fb3546
Emiliano <inversiones.socony@gmail.com>
2022-11-16 21:40:58 -0400

    parameterized routes

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,41 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
+  styleUrls: ['./task-list.component.css'],
 })
 export class TaskListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    var date: Date = new Date(this.route.snapshot.params['date']);
+    console.log(date);
   }
 
   tasks: Task[] = [];
 
-  add(newTask: string){
-    if (newTask) {      
+  add(newTask: string) {
+    if (newTask) {
       this.tasks.push(new Task(newTask));
     }
   }
 
-  remove(existingTask: Task){
-    let confirmed = confirm(`Deseas eliminar el item "${existingTask.title}"?`)
+  remove(existingTask: Task) {
+    let confirmed = confirm(`Deseas eliminar el item "${existingTask.title}"?`);
 
     if (confirmed) {
-      this.tasks = this.tasks.filter(task => task != existingTask);
+      this.tasks = this.tasks.filter((task) => task != existingTask);
     }
   }
 }
 
 class Task {
-
   public isDone = false;
 
-  constructor(public title: string){}
+  constructor(public title: string) {}
 
-  toggleIsDone(){
-    this.isDone = !this.isDone
+  toggleIsDone() {
+    this.isDone = !this.isDone;
   }
 }
